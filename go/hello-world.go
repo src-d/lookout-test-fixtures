5fe468b62112e69bae390051e990271f7b1cc294
Carlos Martín <carlos.martin.sanchez@gmail.com>
2018-09-05 16:37:03 +0100

    Add comment to hello-world.go
    Signed-off-by: Carlos Martín <carlos.martin.sanchez@gmail.com>

--- go/hello-world.go
+++ go/hello-world.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
+	// An important (long) comment. An important (long) comment. An important (long) comment. An important (long) comment.
 	fmt.Println("hello world")
 }
