60ab15bcf78350679574fd190dae75145932f13d
Maxim Sukharev <maxim@sourced.tech>
2018-09-05 19:03:11 +0200

    first modify file for incremental update
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- dummy.go
+++ dummy.go
@@ -4,5 +4,5 @@ package fixtures
 
 // NoopYes is a function that does absolutely nothing but returns true. I need to make this comment long enough so it would be catched by any linter that doesn't like long lines.
 func NoopYes() bool {
-	return true
+	return false
 }
