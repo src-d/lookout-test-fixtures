72745c8f112cff929927dd12a3decf45239c30e2
Maxim Sukharev <maxim@sourced.tech>
2018-09-06 11:26:51 +0200

    merge conflict
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

---
+++ dummy.go
@@ -0,0 +1,8 @@
+// this file should conflict with master branch
+
+package fixtures
+
+// NoopNo is a function that does absolutely nothing but returns false. I need to make this comment long enough so it would be catched by any linter that doesn't like long lines.
+func NoopNo() bool {
+	return false
+}
